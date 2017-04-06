24f43d6a4f8035cf12dbecd02335fc691d0590c4
JulienDERONGS <julien.derongs@masagroup.net>
2017-04-06 16:41:33 +0200

    Site * jQuery * jQuery-ui

--- Website/index.php
+++ Website/index.php
@@ -1,5 +1,30 @@
+<!DOCTYPE html>
 <html>
+    <head>
+        <script scr='include/js/jquery-3.2.0.js'></script>
+        <script scr='include/js/jquery-ui-1.12.1/jquery-ui.js'></script>
+        <meta charset="UTF-8">
+        <link rel="shortcut icon" href="favicon.ico"/>
+        <title>Sleipnir equipments</title>
+        <link rel="stylesheet" type="text/css" href="include/css/sleipnir_css.css">
+
+        
+    </head>
     <body>
-    Hello worlb 111
+        
+    <div class="title">Sleipnir</div>
+    
+    <button class="login_btn">BTN</button>
+
+
+
+
+
+    <script language="JavaScript" type="text/javascript"> 
+        $( document ).ready(function() {
+            console.log( "ready!" );
+        });
+    </script>
+
     </body>
 </html>
