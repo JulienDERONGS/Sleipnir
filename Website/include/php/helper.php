4e5e1bb5df0e662a83ca0b39cf66e96ad039981d
JulienDERONGS <julien.derongs@masagroup.net>
2017-04-23 09:53:35 +0200

    Transfert

--- Website/include/php/helper.php
+++ Website/include/php/helper.php
@@ -1,81 +1,80 @@
 <?php
-namespace HelperNamespace
-{
+namespace HelperClasses;
     /* ========== PHP Helper ========== */
             /*
             - directories' & files' paths
             - utility functions'
             */
-    class Helper
+$var111 = 12;
+class Helper
+{
+    /* ---------- Variables ---------- */
+    private     $root_path;
+    private     $sleipnir_ini_path;
+    protected   $sleipnir_settings = array();
+
+    public      function __construct ()
     {
-        /* ---------- Variables ---------- */
-        private     $root_path;
-        private     $sleipnir_ini_path;
-        protected   $sleipnir_settings = array();
+        // Get paths
+        $this->$root_path = "/var/www/html";
+        $this->$sleipnir_ini_path = $root_dir . "/include/config/sleipnir_settings.ini";
 
-        public      function __construct ()
-        {
-            // Get paths
-            $this->$root_path = "/var/www/html";
-            $this->$sleipnir_ini_path = $root_dir . "/include/config/sleipnir_settings.ini";
-
-            // Parse the sleipnir_settings.ini into a protected array
-            $sleipnir_settings[] = parse_ini_file($sleipnir_settings);
-        }
-
-        /* ---------- Methods ---------- */
-        public      function    __destruct (){}
+        // Parse the sleipnir_settings.ini into a protected array
+        $sleipnir_settings[] = parse_ini_file($sleipnir_settings);
     }
 
-    class User extends Helper
+    /* ---------- Methods ---------- */
+    public      function    __destruct (){}
+}
+
+class User extends Helper
+{
+    /* ---------- Variables ---------- */
+    private     $email;
+    private     $password;
+    private     $level;
+
+    /* ---------- Methods ---------- */
+    public      function __construct ($def_email, $def_password, $def_level)
     {
-        /* ---------- Variables ---------- */
-        private     $email;
-        private     $password;
-        private     $level;
-
-        /* ---------- Methods ---------- */
-        public      function __construct ($def_email, $def_password, $def_level)
-        {
-            $email = $def_email;
-            $password = $def_password;
-            $level = $def_level;
-            echo "User created with consulting rights (code only)";
-        }
-
-        public      function __destruct (){}
+        $email = $def_email;
+        $password = $def_password;
+        $level = $def_level;
+        echo "User created with consulting rights (code only)";
     }
 
-    class Database extends Helper
+    public      function __destruct (){}
+}
+
+class Database extends Helper
+{
+    /* ---------- Variables ---------- */
+    private     $slq_host;
+    private     $sql_login;
+    private     $sql_password;
+
+    /* ---------- Methods ---------- */
+    public      function __construct ()
     {
-        /* ---------- Variables ---------- */
-        private     $slq_host;
-        private     $sql_login;
-        private     $sql_password;
-
-        /* ---------- Methods ---------- */
-        public      function __construct ()
-        {
-            /// PARSER
-            $this->$slq_host = "host";
-            $this->$sql_login = "login";
-            $this->$sql_password = "password";
-            echo "Generic constructor used";
-        }
-
-        public      function __destruct (){}
-        
-        protected   function    getSQLHost()
-        {
-            return $slq_host;
-        }
-
-        protected   function    setSQLHost($new_sql_host)
-        {
-            $sql_host = $new_sql_host;
-        }
-
-        //$test[] = parse_ini_file($file);
+        /// PARSER
+        $this->$slq_host = "host";
+        $this->$sql_login = "login";
+        $this->$sql_password = "password";
+        echo "Generic constructor used";
     }
+
+    public      function __destruct (){}
+    
+    protected   function    get_sql_host()
+    {
+        return $slq_host;
+    }
+
+    protected   function    set_sql_host($new_sql_host)
+    {
+        $sql_host = $new_sql_host;
+    }
+
+    //$test[] = parse_ini_file($file);
 }
 ?>
--- Website/index.php
+++ Website/index.php
@@ -2,20 +2,21 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <?php require_once('/var/www/html/include/php/helper.php'); ?>
+        
         <link rel="shortcut icon" href="favicon.ico"/>
         <link rel="stylesheet" type="text/css" href="include/css/sleipnir_css.css">
-
+        <?php require_once('include/php/helper.php'); ?>
         <title>Sleipnir equipments</title>
     </head>
 
     <body class="content">
-
         <!-- Header : Sticky navigation bar -->
         <?php require_once('include/header/header.php'); ?>
-        
+        <?php echo "pouet"; ?>
         <!-- Include the helper -->
-        <?php use HelperNamespace; ?>
+        <?php
+        use HelperClasses;
+        ?>
 
         <!-- Sticky title -->
         <div class="sticky_title">HOME</div>
@@ -25,18 +26,15 @@
             Welcome to the Sleipnir's equipments' managment page
         </div>
 
-
-
-
         <div><?php
-            $helper = new HelperNamespace\Helper();
+            //$helper = new HelperClasses\Database();
+            //$helper->set_sql_host("test");
+            //echo $helper->get_sql_host();
             
-            $helper->setSQLHost("testHELPER OK !");
-            echo $helper->getSQLHost();
-            
-            
+            echo HelperClasses/$var111;
         ?></div>
-
-        </script>
+        <div>
+        	<?php echo phpinfo(); ?>
+        </div>
     </body>
 </html>
